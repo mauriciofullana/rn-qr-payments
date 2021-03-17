afcd0dafad1c7e1d8af33c69e4d79f050aa953a2
Mauricio Fullana <mfullana4@gmail.com>
2021-03-16 23:09:02 -0300

    Add Roboto google fonts

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_300Light_Italic,
+	Roboto_900Black,
+	Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 import store from './src/state';
 import Navigator from './src/navigation';
 import { CommonResult, Spinner } from './src/components';
 
 export default function App() {
+	let [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_300Light_Italic,
+		Roboto_900Black,
+		Roboto_700Bold,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
 	return (
 		<Provider store={store}>
 			<Spinner />
